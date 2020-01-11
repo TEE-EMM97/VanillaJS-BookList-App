ceebe5499052b64463c83f255a8ac0b29928d8f4
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-01-11 19:55:50 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//Book Class: Represents a Book 
+class Book {
+    //method to take in all the fields and assign to property of object.
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+//UI Class: Hamdle UI Tasks
+class UI {
+    static displayBooks(){
+      const StoreBooks = [
+      {
+         title: 'Book One',
+         author: 'John Doe',
+         isbn: '3434434' 
+      },
+      {
+         title: 'Book One',
+         author: 'John Doe',
+         isbn: '3434434'  
+      }
+      ]; 
+      
+      const books = StoreBooks;
+
+      books.forEach((book) => UI.addBookToList(book));      
+    }
+    
+    static addBookToList(book){
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    };
+}
+
+//Store Class: Handles Storage
+
+//Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+//Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    
+    // Prevent actual submit
+    e.preventDefault();
+    
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    console.log(book);
+});
+//Event: Remove a Book
